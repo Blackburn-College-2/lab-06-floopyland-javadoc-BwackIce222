09c942525944b9b3fc4610815b0bada0157c3b8d
Montreal Thomas <montreal.thomas@blackburn.edu>
2020-03-05 10:52:36 -0600

    first push

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"FloopyLand","l":"BaseHero"},{"p":"FloopyLand","l":"Controller"},{"p":"FloopyLand","l":"GameSquare"},{"p":"FloopyLand","l":"Item"},{"p":"FloopyLand","l":"Runner"}]
